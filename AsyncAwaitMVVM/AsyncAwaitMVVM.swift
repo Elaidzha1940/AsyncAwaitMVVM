730404d4c56e541ab8310a46569275798da63996
Elaidzha1940 <eli080293@gmail.com>
2024-01-08 17:37:34 +0300

    Update AsyncAwaitMVVMViewModel

--- AsyncAwaitMVVM/AsyncAwaitMVVM.swift
+++ AsyncAwaitMVVM/AsyncAwaitMVVM.swift
@@ -9,11 +9,40 @@
 
 import SwiftUI
 
+final class MyManagerClass {
+
+    func getData() async throws -> String {
+        "Some data."
+    }
+}
+
+actor MyManagerActor {
+    
+    func getData() async throws -> String {
+        "Some data."
+    }
+}
+
+final class AsyncAwaitMVVMViewModel: ObservableObject {
+    let mangerClass = MyManagerClass()
+    let managerActor = MyManagerActor()
+    
+    func onCallToActionButtonPressed() {
+        Task {
+            
+        }
+    }
+}
+
 struct AsyncAwaitMVVM: View {
+    @StateObject private var viewModel = AsyncAwaitMVVMViewModel()
+    
     var body: some View {
         
         VStack {
-          
+            Button("Press me") {
+                viewModel.onCallToActionButtonPressed()
+            }
         }
     }
 }

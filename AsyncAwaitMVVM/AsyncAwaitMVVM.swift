2af73c98cb590a21e4a6992b6764520d755b9f6f
Elaidzha1940 <eli080293@gmail.com>
2024-01-08 17:27:51 +0300

    AsyncAwaitMVVM

---
+++ AsyncAwaitMVVM/AsyncAwaitMVVM.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: AsyncAwaitMVVM
+//  File: AsyncAwaitMVVM.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.01.2024
+//
+//  */
+
+import SwiftUI
+
+struct AsyncAwaitMVVM: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    AsyncAwaitMVVM()
+}
---
+++ AsyncAwaitMVVM/AsyncAwaitMVVMApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: AsyncAwaitMVVM
+//  File: AsyncAwaitMVVMApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.01.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct AsyncAwaitMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AsyncAwaitMVVM()
+        }
+    }
+}
